4e869f567758756558b857eec73195a1c08c2059
Dan Kiuna <dkkiuna11@gmail.com>
2023-01-12 15:34:24 -0600

    bootstrap aws-amplify

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sample Vite + React + Amplify app
+
+Categories are isolated by branch
+
+### Authentication (auth)
+
+### PubSub (pubsub)
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { Amplify } from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
