99a1e88ca43d2910f486b8f5f7cc65bbb7f89428
Dan Kiuna <dkkiuna11@gmail.com>
2023-05-01 12:44:43 -0500

    11308

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "StorageCategory": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "storage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,94 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Storage, Auth } from 'aws-amplify'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [user, setUser] = useState({})
+  const [formcontent, setformContent] = useState('test')
+
+  const signIn = async () => {
+    try {
+      const _user = await Auth.signIn(user.email, user.password)
+      console.log(_user)
+    } catch (error) {
+      console.log('error signing in', error)
+    }
+  }
+
+  const signUp = async () => {
+    try {
+      const _user = await Auth.signUp({
+        username: user.email,
+        password: user.password,
+      })
+      console.log(_user)
+    } catch (error) {
+      console.log('error signing up:', error)
+    }
+  }
+
+
+  const confirm = async () => {
+    try {
+      const _user = await Auth.confirmSignUp(user.email, user.code)
+      console.log(_user)
+    } catch (error) {
+      console.log('error confirming sign up', error)
+    }
+  }
+
+
+  const uploadObject = async (key, content, level, type) => {
+    const blob = new Blob([content], { type });
+    // const {username} = await Auth.currentAuthenticatedUser();
+    // console.log(username)
+    //   return new Promise((res, rej) => {
+    //       Storage.put(key, blob, {
+    //           level: level,
+    //           contentType: type,
+    //           resumable: true,
+    //           metadata: {
+    //               username: 'anon'
+
+    //           },
+    //           completeCallback: (event) => {
+    //               console.log('complete',event);
+    //               res(event);
+    //           },
+    //           errorCallback: (err) => {
+    //               console.error(err);
+    //               rej(new Error('We could not complete that action. Please try again'));
+    //           },
+    //           progressCallback: (progress) => {
+    //               console.log('....',progress);
+    //           }
+    //       });
+    // });
+    try {
+      const res = await Storage.put(key, blob, {
+        level: level,
+        contentType: type,
+        resumable: true,
+        metadata: {
+          username: 'anon'
+        },
+        progressCallback: (progress) => {
+          console.log('....', progress);
+        },
+        completeCallback: (event) => {
+          console.log('complete', event);
+        },
+        errorCallback: (err) => {
+          console.error(err);
+        }
+      });
+      console.log(res);
+    } catch (error) {
+      console.log('SOME ERROR', error);
+    }
+  };
 
   return (
     <div className="App">
@@ -17,9 +102,20 @@ function App() {
       </div>
       <h1>Vite + React + Amplify</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <input type="text" onChange={(e) => setUser({...user, email: e.target.value})} /><br />
+        <input type="password" onChange={(e) => setUser({...user, password: e.target.value})} /><br />
+        <button onClick={signIn}>Sign In</button><br />
+        <button onClick={signUp}>Sign Up</button><br />
+        <input type="text" onChange={(e) => setUser({...user, code: e.target.value})} /><br />
+        <button onClick={confirm}>Confirm</button><br />
+        {JSON.stringify(user,null,2)}
+        <hr />
+        <button onClick={ async () => console.log(await Auth.currentAuthenticatedUser())}>Current User</button><br />
+        <input type="file" onChange={(e) => {console.log(e.target.files[0]);setformContent(e.target.files[0])}} /><br />
+        <button onClick={() => uploadObject(formcontent.name, formcontent, 'public', formcontent.type)}>Upload</button><br />
+        <hr />
+        {/* <button onClick={() => uploadObject('test.txt', 'test', 'public', 'text/plain')}>Upload</button><br /> */}
+        <button onClick={async () => await Auth.currentCredentials() }>Credentials</button><br />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -32,3 +128,28 @@ function App() {
 }
 
 export default App
+
+
+/**
+ * 
+ * {
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Effect": "Allow",
+			"Principal": {
+				"Federated": "cognito-identity.amazonaws.com"
+			},
+			"Action": "sts:AssumeRoleWithWebIdentity",
+			"Condition": {
+				"StringEquals": {
+					"cognito-identity.amazonaws.com:aud": "us-east-2:e563811f-4226-4166-b68c-1781d8b6b676"
+				},
+				"ForAnyValue:StringLike": {
+					"cognito-identity.amazonaws.com:amr": "unauthenticated"
+				}
+			}
+		}
+	]
+}
+ */
