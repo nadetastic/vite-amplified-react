026e0b0316d913d2d4dcbcc14eb5c3012a64b1fa
Dan Kiuna <dkkiuna11@gmail.com>
2023-05-18 15:01:42 -0500

    react router && Test page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App'
+import Test from './Test';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/test",
+    element: <Test />,
+  },
+]);
+
 
 import { Amplify } from 'aws-amplify'
 import config from './aws-exports'
@@ -9,6 +26,6 @@ Amplify.configure(config)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
