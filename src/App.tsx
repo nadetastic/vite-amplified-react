4422c0ab550f60e746b6db982fde512e1c95d0dc
Dan Kiuna <dkkiuna11@gmail.com>
2024-01-22 12:16:09 -0600

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
+import { Storage, Auth } from "aws-amplify";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [userForm, setUserForm] = useState<any>({});
+
+  const fileUpload = async (file: any) => {
+    //}, progressSetter: any) => {
+    await Storage.put(`path`, file, {
+      progressCallback(progress) {
+        console.log(`uploaded: ${progress.loaded}/${progress.total}`);
+        // progressSetter(Math.round((progress.loaded / progress.total) * 100));
+      },
+    })
+      .then((result) => {
+        // handling the result
+        console.log("result: ", result);
+      })
+      .catch((err) => {
+        // handling the error
+        console.error("error: ", err);
+      });
+  };
+
+  const login = async () => {
+    try {
+      const result = await Auth.signIn(userForm.username, userForm.password);
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <>
@@ -18,9 +48,25 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <input
+          onChange={(e) =>
+            setUserForm({ ...userForm, username: e.target.value })
+          }
+        />
+        <input
+          onChange={(e) =>
+            setUserForm({ ...userForm, password: e.target.value })
+          }
+        />
+        <button onClick={login}>Login</button>
+        <br />{" "}
+        <input
+          type="file"
+          // accept=".mp4, .m4v, .mov"
+          onChange={(e) => {
+            fileUpload(e.target.files![0]);
+          }}
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +75,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
