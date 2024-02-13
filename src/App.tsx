8d4464fe4c6a3c56d6b715d8db3a95e509ee2acb
Dan Kiuna <dkkiuna11@gmail.com>
2024-02-12 22:34:25 -0600

    progress test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+// import { generateClient } from "aws-amplify/api";
+import { Storage } from "aws-amplify";
+// import * as queries from "./graphql/queries";
+// export const client = generateClient();
 function App() {
-  const [count, setCount] = useState(0)
+  // const [file, setFile] = useState();
+
+  const fileUpload = async (file) => {
+    await Storage.put(`path`, file, {
+      progressCallback(progress) {
+        console.log(`progress uploaded: ${progress.loaded}/${progress.total}`);
+      },
+    })
+      .then((result) => {
+        // handling the result
+        console.log("result: ", result);
+      })
+      .catch((err) => {
+        // handling the error
+        console.error("error: ", err);
+      });
+  };
 
   return (
     <>
@@ -17,19 +38,26 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <div className="card">
+            {user?.username}
+            <input
+              type="file"
+              onChange={(e) => fileUpload(e.target.files[0])}
+            />
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+            <button onClick={signOut}>Sign Out</button>
+          </div>
+        )}
+      </Authenticator>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Amplify } from "aws-amplify";
+import config from "./amplifyconfiguration.json";
+// import { fetchAuthSession } from "aws-amplify/auth";
+// import { cognitoUserPoolsTokenProvider } from "aws-amplify/auth/cognito";
+// import { defaultStorage, CookieStorage } from "aws-amplify/utils";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const configureAmplify = () => {
+  // Amplify.configure({
+  //   Auth: {
+  //     Cognito: {
+  //       identityPoolId: config.aws_cognito_identity_pool_id,
+  //       userPoolId: config.aws_user_pools_id,
+  //       userPoolClientId: config.aws_user_pools_web_client_id,
+  //       loginWith: {
+  //         oauth: {
+  //           domain: config.oauth.domain,
+  //           scopes: ["email", "profile", "openid"],
+  //           redirectSignIn: ["http://localhost:3000/"],
+  //           redirectSignOut: ["http://localhost:3000/"],
+  //           responseType: "code",
+  //         },
+  //       },
+  //     },
+  //   },
+  // });
+
+  Amplify.configure(config);
+
+  // cognitoUserPoolsTokenProvider.setKeyValueStorage(new CookieStorage());
+};
+
+configureAmplify();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
