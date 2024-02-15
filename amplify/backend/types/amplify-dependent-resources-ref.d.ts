b0877765ee4c69f5e757bde0408028aeabe35438
Dan Kiuna <dkkiuna11@gmail.com>
2024-02-15 15:26:28 -0600

    saving progress

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "authfc6ca971fc6ca971": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import { Authenticator } from "@aws-amplify/ui-react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
 
+import {
+  signIn,
+  signUp,
+  confirmSignUp,
+  getCurrentUser,
+  fetchMFAPreference,
+  fetchDevices,
+} from "aws-amplify/auth";
 function App() {
-  const [count, setCount] = useState(0)
+  const handleSignIn = async () => {
+    try {
+      const result = await signIn({
+        username: "dkkiuna11+2@gmail.com",
+        password: "abcd1234",
+      });
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const handleSignUp = async () => {
+    try {
+      const result = await signUp({
+        username: "dkkiuna11+2@gmail.com",
+        password: "abcd1234",
+        options: {
+          userAttributes: {
+            phone_number: "+14695442049",
+          },
+        },
+      });
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleCSignUp = async () => {
+    try {
+      const result = await confirmSignUp({
+        confirmationCode: "096088",
+        username: "dkkiuna11+2@gmail.com",
+      });
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const currentUser = async () => {
+    try {
+      const result = await getCurrentUser();
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const fetchMFA = async () => {
+    console.log(Amplify.getConfig().Auth?.Cognito);
+    try {
+      const result = await fetchMFAPreference();
+      // const result = await fetchDevices();
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
-    <>
+    <Authenticator>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,9 +87,21 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <div>
+          <button onClick={handleSignIn}>Sign In</button>
+        </div>
+        <div>
+          <button onClick={handleSignUp}>Sign Up</button>
+        </div>
+        <div>
+          <button onClick={handleCSignUp}>Confirm Sign Up</button>
+        </div>
+        <div>
+          <button onClick={currentUser}>Current User</button>
+        </div>
+        <div>
+          <button onClick={fetchMFA}>Fetch MFA</button>
+        </div>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -28,8 +109,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </Authenticator>
+  );
 }
 
-export default App
+export default App;
