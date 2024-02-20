8081a90e435c39272924dc19d7141a5d406c5285
Dan Kiuna <dkkiuna11@gmail.com>
2024-02-20 11:03:36 -0600

    saving

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "auth9328b7a19328b7a1": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
